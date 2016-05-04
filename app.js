30fa9a82a532c19cc3a3e9fedec2d852de6144bf
Asset Mamyraimov <Asset Mamyraimov>
2016-05-04 13:19:04 +0600

    add pubdate and moment.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
-var yahooSql = 'select title,link,description from rss where url="http://feeds.feedburner.com/meduza/biLG?format=xml"';
+var yahooSql = 'select title,pubDate,link,description from rss where url="http://feeds.feedburner.com/meduza/biLG?format=xml"';
 var meduzaRss =  'https://query.yahooapis.com/v1/public/yql?q='+encodeURI(yahooSql)+'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+moment.locale('ru');
 function getAmpLink(link){
     //remove protocol
     var cleanUrl = link.replace(/.*?:\/\//g, "");
     return 'https://amp.'+cleanUrl;
+}
+function updateDate(){
+    $('.pubdate span').each(function() {
+        var now = moment();
+
+            $(this).text(moment($(this).text()).format('DD.MM.YYYY, HH:mm'));
+
+    });
 }
 $.getJSON(meduzaRss, function(data) {
     var items = data.query.results.item;
@@ -14,9 +23,11 @@ $.getJSON(meduzaRss, function(data) {
             link = getAmpLink(item.link);
             var item_block = '<div class="item">' +
                 '<a href="'+link+'" class="title" target="_blank">'+item.title+'</a>' +
+                 '<div class="pubdate">Дата публикации: <span>'+item.pubDate+'</span></div>'+
                 '<div class="body">'+item.description+'</div>' +
                 '</div>';
             $('.content').append(item_block);
+            updateDate();
         });
     }
 
